07992a135846afb170609e62eebf7d13c78f9fad
Joseph Turian <turian@gmail.com>
2021-08-01 14:04:38 +0200

    Sox transforms, and save JSON

--- transforms.py
+++ transforms.py
@@ -26,22 +26,22 @@ def note_to_freq(note):
 # (transform name, [(continuous parameter, min, max), ...], [(categorical parameter, [values])])
 # TODO: Add other transforms from JND paper?
 transforms = [
-    ("allpass", [("midi", 0, 127), ("width_q", 0.01, 5)], []),
+    ("sox", "allpass", [("midi", 0, 127), ("width_q", 0.01, 5)], []),
     (
-        "bandpass",
+        "sox", "bandpass",
         [("midi", 0, 127), ("width_q", 0.01, 5)],
         [("constant_skirt", [True, False])],
     ),
-    ("bandreject", [("midi", 0, 127), ("width_q", 0.01, 5)], []),
-    ("bass", [("gain_db", -20, 20), ("midi", 0, 127), ("slope", 0.3, 1.0)], []),
+    ("sox", "bandreject", [("midi", 0, 127), ("width_q", 0.01, 5)], []),
+    ("sox", "bass", [("gain_db", -20, 20), ("midi", 0, 127), ("slope", 0.3, 1.0)], []),
     # bend
     (
-        "chorus",
+        "sox", "chorus",
         [("gain_in", 0.0, 1.0), ("gain_out", 0.0, 1.0)],
         [("n_voices", [2, 3, 4])],
     ),
     (
-        "compand",
+        "sox", "compand",
         [
             ("attack_time", 0.01, 1.0),
             ("decay_time", 0.01, 2.0),
@@ -49,17 +49,17 @@ transforms = [
         ],
         [],
     ),
-    ("contrast", [("amount", 0, 100)], []),
+    ("sox", "contrast", [("amount", 0, 100)], []),
     # delays and decays and n_echos
-    ("echo", [("gain_in", 0, 1), ("gain_out", 0, 1)], []),
-    ("equalizer", [("midi", 0, 127), ("gain_db", -20, 20), ("width_q", 0.01, 5)], []),
+    ("sox", "echo", [("gain_in", 0, 1), ("gain_out", 0, 1)], []),
+    ("sox", "equalizer", [("midi", 0, 127), ("gain_db", -20, 20), ("width_q", 0.01, 5)], []),
     (
-        "fade",
+        "sox", "fade",
         [("fade_in_len", 0, 2), ("fade_out_len", 0, 2)],
         [("fade_shape", ["q", "h", "t", "l", "p"])],
     ),
     (
-        "flanger",
+        "sox", "flanger",
         [
             ("delay", 0, 30),
             ("depth", 0, 10),
@@ -71,7 +71,7 @@ transforms = [
         [("shape", ["triangle", "sine"]), ("interp", ["linear", "quadratic"])],
     ),
     (
-        "gain",
+        "sox", "gain",
         [("gain_db", -20, 20)],
         [
             ("normalize", [True, False]),
@@ -79,16 +79,16 @@ transforms = [
             ("balance", [None, "e", "B", "b"]),
         ],
     ),
-    ("highpass", [("midi", 0, 127), ("width_q", 0.01, 5)], [("n_poles", [1, 2])]),
+    ("sox", "highpass", [("midi", 0, 127), ("width_q", 0.01, 5)], [("n_poles", [1, 2])]),
     # loudness: Loudness control. Similar to the gain effect, but
     # provides equalisation for the human auditory system.
-    ("lowpass", [("midi", 0, 127), ("width_q", 0.01, 5)], [("n_poles", [1, 2])]),
+    ("sox", "lowpass", [("midi", 0, 127), ("width_q", 0.01, 5)], [("n_poles", [1, 2])]),
     # mcompand
     # noisered
     # This might be too extreme?
-    ("overdrive", [("gain_db", 0, 100), ("colour", 0, 100)], []),
+    ("sox", "overdrive", [("gain_db", 0, 100), ("colour", 0, 100)], []),
     (
-        "phaser",
+        "sox", "phaser",
         [
             ("gain_in", 0, 1),
             ("gain_out", 0, 1),
@@ -98,10 +98,10 @@ transforms = [
         ],
         [("modulation_shape", ["sinusoidal", "triangular"])],
     ),
-    ("pitch", [("n_semitones", -12, 12)], [("quick", [True, False])]),
+    ("sox", "pitch", [("n_semitones", -12, 12)], [("quick", [True, False])]),
     # rate
     (
-        "reverb",
+        "sox", "reverb",
         [
             ("reverberance", 0, 100),
             ("high_freq_damping", 0, 100),
@@ -112,15 +112,15 @@ transforms = [
         ],
         [("wet_only", [True, False])],
     ),
-    ("speed", [("factor", 0.5, 1.5)], []),
-    ("stretch", [("factor", 0.5, 1.5)], [("window", [10, 20, 50])]),
+    ("sox", "speed", [("factor", 0.5, 1.5)], []),
+    ("sox", "stretch", [("factor", 0.5, 1.5)], [("window", [10, 20, 50])]),
     (
-        "tempo",
+        "sox", "tempo",
         [("factor", 0.5, 1.5)],
         [("audio_type", ["m", "s", "l"]), ("quick", [True, False])],
     ),
-    ("treble", [("gain_db", -20, 20), ("midi", 0, 127), ("slope", 0.3, 1.0)], []),
-    ("tremolo", [("speed", 0.1, 10.0), ("depth", 0, 100)], []),
+    ("sox", "treble", [("gain_db", -20, 20), ("midi", 0, 127), ("slope", 0.3, 1.0)], []),
+    ("sox", "tremolo", [("speed", 0.1, 10.0), ("depth", 0, 100)], []),
 ]
 
 
@@ -135,31 +135,39 @@ def choose_value(name, low, hi):
 
 def transform_file(f):
     transform_spec = random.choice(transforms)
-    transform = transform_spec[0]
+    transform = "%s-%s" % (transform_spec[0], transform_spec[1])
     params = {}
     print(transform_spec)
-    for param, low, hi in transform_spec[1]:
+    for param, low, hi in transform_spec[2]:
         param, v = choose_value(param, low, hi)
         params[param] = v
-    for param, vals in transform_spec[2]:
+    for param, vals in transform_spec[3]:
         params[param] = random.choice(vals)
 
-    outfiles = []
+    #outfiles = []
     # TODO: Save JSON of all transforms
     slug = f"{os.path.split(f)[1]}-{transform}-{hashlib.sha224(json.dumps(params, sort_keys=True).encode('utf-8')).hexdigest()[:4]}"
+    print(slug)
     outf = os.path.join(
-        "data/transforms",
-        os.path.split(os.path.split(f)[0])[1],
+        #"data/transforms",
+        "gold/transforms",
+        #os.path.split(os.path.split(f)[0])[1],
         os.path.split(f)[1],
         f"{slug}.ogg",
     )
+    outjson = os.path.splitext(outf)[0] + ".json"
     outd = os.path.split(outf)[0]
     if not os.path.exists(outd):
         os.makedirs(outd)
-    outfiles.append(outf)
-    tfm = sox.Transformer()
-    tfm.__getattribute__(transform)(**params)
-    tfm.build_file(f, outf)
+    #outfiles.append(outf)
+    if transform_spec[0] == "sox":
+        tfm = sox.Transformer()
+        tfm.__getattribute__(transform_spec[1])(**params)
+        tfm.build_file(f, outf)
+    else:
+        assert False, f"Unknown transformer {transform_spec[0]}"
+    open(outjson, "wt").write(json.dumps(
+        [{"orig": f}, {transform: params}], indent=4))
 
     """
     # TODO: Different openl3 model?
@@ -174,4 +182,5 @@ def transform_file(f):
     """
 
 for f in tqdm(list(glob.glob("data/preprocessed/*/*.ogg"))):
+#for f in tqdm(list(glob.glob("gold/preprocessed/*.ogg"))):
     transform_file(f)
