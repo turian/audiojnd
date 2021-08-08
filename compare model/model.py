61af371acc08623a91779322c88d9f394ef19103
Gyanendra <gyanendralucky9337@gmail.com>
2021-08-08 22:39:16 +0530

    Dataset Prepare

--- compare model/model.py
+++ compare model/model.py
@@ -16,21 +16,26 @@ class AudioJNDModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = torchopenl3.models.load_audio_embedding_model(
-            input_repr="mel256", content_type="music", embedding_size=6144
+            input_repr="mel256", content_type="env", embedding_size=6144
         )
         self.scale = ScaleLayer()
         self.cos = nn.CosineSimilarity(eps=1e-6)
 
     def forward(self, x1, x2):
-        bs, in1, in2, in3 = x1.size()
+        bs, _, in2, in3 = x1.size()
+
         x1 = self.model(x1.view(-1, in2, in3))
         x2 = self.model(x2.view(-1, in2, in3))
+
         x1 = self.scale(x1).view(bs, -1)
         x2 = self.scale(x2).view(bs, -1)
+
         prob = self.cos(x1, x2)
         assert torch.all((prob < 1) & (prob > -1))
+
         prob = (prob + 1) / 2
         assert torch.all((prob < 1) & (prob > 0))
+
         return prob
 
 
