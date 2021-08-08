abf76806478ef01a1bb81d5245315f2bc20b69a9
Joseph Turian <turian@gmail.com>
2021-08-08 18:38:41 +0200

    Basic retrain script

---
+++ retrain.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+"""
+Given training annotations, retrain the model.
+Evaluate using 5-fold random split.
+"""
+
+import csv
+import glob
+import os.path
+import re
+from typing import Optional
+
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
+
+from preprocess import LENGTH_SAMPLES, ensure_length
+from transforms import CONFIG, pydubread
+
+FOLDS = 5
+LENGTHRE = re.compile(".*\.wav-([0-9\.]+)\..*")
+
+
+class PairedDatset(Dataset):
+    def __init__(self, rows):
+        self.rows = rows
+
+    def __getitem__(self, idx):
+        oldf, newf, y = self.rows[idx]
+        print(oldf, newf)
+        import sys
+
+        sys.stdout.flush()
+
+        # This is copied from modelscores.py,
+        # maybe make this a util
+        if LENGTHRE.match(newf):
+            length = float(LENGTHRE.match(newf).group(1))
+            nsamples = dict(LENGTH_SAMPLES)[length]
+        else:
+            assert False
+
+        oldx = ensure_length(pydubread(oldf), nsamples, from_start=True)
+        newx = ensure_length(pydubread(newf), nsamples, from_start=True)
+        assert oldx.shape == newx.shape, f"{oldx.shape} != {newx.shape}"
+        assert oldx.shape == (nsamples,)
+
+        print(oldx.shape, newx.shape)
+        sys.stdout.flush()
+        return oldx, newx, y
+
+    def __len__(self):
+        return len(self.rows)
+
+
+class AnnotationsDataModule(pl.LightningDataModule):
+    # batch_size = 1 because we have two different audio lengths :\
+    # Otherwise we could try writing our own collate_fn
+    # There might also be a way to interleave batches from two datasets
+    def __init__(self, data_dir: str = "data/iterations", batch_size: int = 1):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+
+    def setup(self, stage: Optional[str] = None):
+        self.rows = []
+        print(os.path.join(self.data_dir, "*/annotation*csv"))
+        for csvfile in glob.glob(os.path.join(self.data_dir, "*/annotation*csv")):
+            print(csvfile)
+            for infile, transfile, y in csv.reader(open(csvfile)):
+                y = int(y)
+                if infile == transfile:
+                    continue
+                # Only use dev, not eval
+                if "FSD50K.eval_audio" in infile:
+                    continue
+                self.rows.append((infile, transfile, y))
+        print(len(self.rows))
+        self.dataset = PairedDatset(self.rows)
+        num, div = len(self.rows), FOLDS
+        fold_idx = [num // div + (1 if x < num % div else 0) for x in range(div)]
+        self.folds = random_split(
+            self.dataset, fold_idx, generator=torch.Generator().manual_seed(42)
+        )
+        # TODO: This dataloader won't allow kfold
+
+    def train_dataloader(self):
+        return DataLoader(ConcatDataset(self.folds[1:]), batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.folds[0], batch_size=self.batch_size)
+
+
+#    def test_dataloader(self):
+#        return DataLoader(self.mnist_test, batch_size=self.batch_size)
+
+
+class LitAutoEncoder(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3)
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28)
+        )
+
+    def forward(self, x):
+        # in lightning, forward defines the prediction/inference actions
+        embedding = self.encoder(x)
+        return embedding
+
+    def training_step(self, batch, batch_idx):
+        # training_step defined the train loop.
+        # It is independent of forward
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = F.mse_loss(x_hat, x)
+        # Logging to TensorBoard by default
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+
+def retrain():
+    annotations_data_module = AnnotationsDataModule()
+
+    model = LitAutoEncoder()
+
+    trainer = pl.Trainer()
+    trainer.fit(model, annotations_data_module)
+
+
+if __name__ == "__main__":
+    retrain()
